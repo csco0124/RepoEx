37a6c254d0be29dec17300e146ca058f1acb3d48
dgkim2 <dgkim@bsgglobal.com>
2022-05-27 14:40:32 +0900

    no message

---
+++ vue-project/src/views/DataBinding.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <textarea v-model="message"></textarea>
+  </div>
+</template>
+<script>
+  export default {
+  data() {
+    return {
+      message: 'efewfwefewf'
+    };
+  }
+  }
+</script>

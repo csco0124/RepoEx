de4175d4bf19551ba17c80fe21bf9b43f968580d
dgkim2 <dgkim@bsgglobal.com>
2022-05-30 16:55:05 +0900

    no message

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DataBinding from '../views/DataBinding.vue'
+import ParentComponent from '../views/ParentComponent.vue'
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     path: '/databinding',
     name: 'DataBinding',
     component: DataBinding
+  },
+  {
+    path: '/comp',
+    name: 'ParentComponent',
+    component: ParentComponent
   }
 ]
 

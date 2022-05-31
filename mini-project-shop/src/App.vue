764ae31fce651c8d71f8922da674e8838c52a393
dgkim2 <dgkim@bsgglobal.com>
2022-05-31 19:03:49 +0900

    no message

--- mini-project-shop/src/App.vue
+++ mini-project-shop/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./layouts/Header.vue"
+import Footer from "./layouts/Footer.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header, Footer
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

f9a109152479b4a2081c98dfe305027868ae30bc
dgkim2 <dgkim@bsgglobal.com>
2022-06-07 17:11:42 +0900

    no message

--- spring_jpa/spring-demo/src/main/java/com/example/SpriongDemoApplication.java
+++ spring_jpa/spring-demo/src/main/java/com/example/SpriongDemoApplication.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpriongDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpriongDemoApplication.class, args);
+	}
+	
+	@GetMapping(value = "/")
+	public String helloWorld() {
+		return "Hello World";
 	}
 
 }

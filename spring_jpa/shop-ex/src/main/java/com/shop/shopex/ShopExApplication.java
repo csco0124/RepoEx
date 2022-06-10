ba83633b385b129c5a7eb2b112960ee506a20f1d
DESKTOP-3LEKA0R\dgk <csco0124@naver.com>
2022-06-11 01:03:39 +0900

    no message

---
+++ spring_jpa/shop-ex/src/main/java/com/shop/shopex/ShopExApplication.java
@@ -0,0 +1,13 @@
+package com.shop.shopex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopExApplication.class, args);
+	}
+
+}

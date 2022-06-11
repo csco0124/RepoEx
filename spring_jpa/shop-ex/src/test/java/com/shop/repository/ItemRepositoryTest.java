0e3f2af1f13d40750dea1743612e0efe5b505812
DESKTOP-3LEKA0R\dgk <csco0124@naver.com>
2022-06-12 02:13:30 +0900

    no message

--- spring_jpa/shop-ex/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ spring_jpa/shop-ex/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.shop.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,28 +16,52 @@ import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 
 @SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
 
-    @Autowired
-    ItemRepository itemRepository;
+	@Autowired
+	ItemRepository itemRepository;
 
-    @PersistenceContext
-    EntityManager em;
+	@PersistenceContext
+	EntityManager em;
 
-    @Test
-    @DisplayName("상품 저장 테스트")
-    public void createItemTest(){
-        Item item = new Item();
-        item.setItemNm("테스트 상품");
-        item.setPrice(10000);
-        item.setItemDetail("테스트 상품 상세 설명");
-        item.setItemSellStatus(ItemSellStatus.SELL);
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now());
-        item.setUpdateTime(LocalDateTime.now());
-        Item savedItem = itemRepository.save(item);
-        System.out.println(savedItem.toString());
-    }
-    
+	@Test
+	@DisplayName("상품 저장 테스트")
+	public void createItemTest() {
+		Item item = new Item();
+		item.setItemNm("테스트 상품");
+		item.setPrice(10000);
+		item.setItemDetail("테스트 상품 상세 설명");
+		item.setItemSellStatus(ItemSellStatus.SELL);
+		item.setStockNumber(100);
+		item.setRegTime(LocalDateTime.now());
+		item.setUpdateTime(LocalDateTime.now());
+		Item savedItem = itemRepository.save(item);
+		System.out.println(savedItem.toString());
+	}
+
+	public void createItemList() {
+		for (int i = 1; i <= 10; i++) {
+			Item item = new Item();
+			item.setItemNm("테스트 상품" + i);
+			item.setPrice(10000 + i);
+			item.setItemDetail("테스트 상품 상세 설명" + i);
+			item.setItemSellStatus(ItemSellStatus.SELL);
+			item.setStockNumber(100);
+			item.setRegTime(LocalDateTime.now());
+			item.setUpdateTime(LocalDateTime.now());
+			Item savedItem = itemRepository.save(item);
+		}
+	}
+
+	@Test
+	@DisplayName("상품명 조회 테스트")
+	public void findByItemNmTest() {
+		this.createItemList();
+		List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+		for (Item item : itemList) {
+			System.out.println(item.toString());
+		}
+	}
+
 }

c9956b1ab76c53012e21963435d64d4f8437d793
DESKTOP-3LEKA0R\dgk <csco0124@naver.com>
2022-06-13 00:05:53 +0900

    no message

--- spring_jpa/shop-ex/src/main/java/com/shop/repository/ItemRepository.java
+++ spring_jpa/shop-ex/src/main/java/com/shop/repository/ItemRepository.java
@@ -4,6 +4,8 @@ package com.shop.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shop.entity.Item;
 
@@ -11,4 +13,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
 	List<Item> findByItemNm(String itemNm);
 	
+	@Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }

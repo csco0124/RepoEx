cb1a0d1cd6f6979651b33e2d5b9a85732d4d82ea
DESKTOP-3LEKA0R\dgk <csco0124@naver.com>
2022-06-19 23:17:29 +0900

    no message

--- spring_jpa/shop-ex/src/main/java/com/shop/config/SecurityConfig.java
+++ spring_jpa/shop-ex/src/main/java/com/shop/config/SecurityConfig.java
@@ -6,9 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +18,7 @@ import com.shop.service.MemberService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 	
 	@Autowired
 	MemberService memberService;
@@ -35,9 +34,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.exceptionHandling()
 		    .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
 		*/
-		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		
-		http.formLogin()
+		/*
+		 *  이 프로젝트에서 인증은 AuthenticationManager를 사용하도록 설정
+		 */
+		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		// UserDetailsService를 구현하고 있는(로그인시 사용자 존재 체크 및 사용자 정보를 리턴하는 코드가 있는) 객체인 memberService를 지정해주며, 비밀번호 암호화를 위해 passwordEncoder()를 지정해줌
+        authenticationManagerBuilder.userDetailsService(memberService).passwordEncoder(this.passwordEncoder());
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+		
+		//http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and()
+			.authenticationManager(authenticationManager)	// 인증은 AuthenticationManager를 사용하도록 설정
+			.formLogin()
 	        .loginPage("/members/login")	// 로그인 페이지 URL
 	        .defaultSuccessUrl("/")			// 로그인 성공시 이동할 URL
 	        .usernameParameter("email")		// 로그인시 사용할 파라미터 name
@@ -47,36 +58,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))	// 로그아웃 URL
 	        .logoutSuccessUrl("/");	// 로그아웃 성공시 이동할 URL
 			
+		
 		return http.build();
 	}
 	
-	/**
-	 * 이 프로젝트에서 인증은 AuthenticationManager를 사용하도록 설정
-	 * @param auth
-	 * @return
-	 * @throws Exception
-	 */
-	/*@Bean
-	public AuthenticationManager filterChain(AuthenticationManagerBuilder auth) throws Exception {
-		// UserDetailsService를 구현하고 있는 객체인 memberService를 지정해주며, 비밀번호 암호화를 위해 passwordEncoder()를 지정해줌
-		auth.userDetailsService(memberService).passwordEncoder(this.passwordEncoder());	
-		return auth.build();
-	}*/
+	@Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(memberService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
-    }
-	
 }

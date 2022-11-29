98bbd02a951d41903b620bb0e7e5884e16608dfd
DongGeonKim <csco0124@naver.com>
2022-11-29 14:25:09 +0900

    14
    14

--- vue-project/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import mixins from "./mixins";
-
-const app = createApp(App).use(router).mixin(mixins).mount("#app");
-
-window.Kakao.init("749ee852ff8d08a9202a172774492887"); //발급 받은 앱키

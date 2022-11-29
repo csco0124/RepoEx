a7fe227a3380325e8b197f0a11794b4d16a9ace0
DongGeonKim <csco0124@naver.com>
2022-11-29 14:25:24 +0900

    15
    15

--- node-project-main/express.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express() 
-
-app.listen(3000, () => { // 3000번 포트로 웹 서버 실행
-  console.log('Server started. port 3000.')
-})
-
--- node-project-main/module.js
+++
@@ -1,7 +0,0 @@
-const {add, minus, defaultNum} = require('./calculator');
-
-console.log(add(7,2));
-console.log(minus(7,2));
-console.log(defaultNum);
-
-console.log(global.console);

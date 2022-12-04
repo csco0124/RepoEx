5ce4b340a6a65c2a6981318d4fa4a7bf8be67f08
dgk <csco0124@naver.com>
2022-12-05 00:32:03 +0900

    1

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import DataBinding from "../views/DataBinding.vue";
 const routes = [
   {
     path: "/",
@@ -13,7 +14,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: AboutView
+    component: AboutView,
+  },
+  {
+    path: "/databinding",
+    name: "DataBinding",
+    component: DataBinding,
   },
 ];
 

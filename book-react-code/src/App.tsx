02eb8209f8e79d0ecf1d42fa01c7b8fe201c668e
dgk <csco0124@naver.com>
2022-12-18 04:09:15 +0900

    12

---
+++ book-react-code/src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { ListItem } from "./components/ListItem";
+import axios from "axios";
+import type { User } from "./types/user";
+
+export const App = () => {
+  // 얻은 사용자 정보
+  const [users, setUsers] = useState<User[]>([]);
+
+  // 화면 표시 시 사용자 정보를 얻는다
+  // ※ 실제로는 엔드포인트가 존재하지 않으므로 주의
+  useEffect(() => {
+    axios
+      .get<User[]>(
+        "https://04b4f544-7554-42de-873b-3ee82d6b5e12.mock.pstmn.io/list"
+      )
+      .then((res) => {
+        setUsers(res.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <ListItem
+          id={user.id}
+          name={user.name}
+          age={user.age}
+          personalColor={user.personalColor}
+          hobbies={user.hobbies}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ book-react-code/src/components/ListItem.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import type { User } from "../types/user";
+
+//export const ListItem = (props:User) => {
+export const ListItem: FC<User> = props => {
+  const { id, name, age, personalColor, hobbies } = props;
+  return (
+    <p style={{color:personalColor}}>
+      {id}:{name}({age}) {hobbies?.join(" / ")}
+    </p>
+  );
+};

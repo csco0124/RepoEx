92501a79e6bbdbd69a0376db3b19879ffe49c12f
dgk <csco0124@naver.com>
2023-01-18 22:50:19 +0900

    12

---
+++ hof-test-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Child from "./Child";
+
+function App() {
+  return (
+    <div>
+      <h2>고차 함수 테스트</h2>
+      <hr />
+      <Child />
+    </div>
+  );
+}
+
+export default App;

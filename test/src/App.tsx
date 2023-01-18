7da42880b295cf76de09c4cafc01ddfb55585257
dgkim <csco0124@naver.com>
2023-01-18 15:33:33 +0900

    12

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Table from "./Table";
+import produce from "immer";
 
 export type Person = {
   name: string;
@@ -10,14 +11,22 @@ export type Person = {
 
 function App() {
   const [person, setPerson] = useState<Person>();
-  const [personList, setPersonList] = useState<Person[]>([{
-    name: "test1",
-    age: 20,
-    email: "test@test.com",
-  }]);
+  const [personList, setPersonList] = useState<Person[]>([{name: "test1",age: 20,email: "test@test.com",},
+  ]);
+
+  const addPerson = () => {
+    const personObj: Person = {name: "test2",age: 22,email: "test2@test.com",};
+    //const newPersonList = [...personList, personObj];   // 전개연산자 활용
+    const newPersonList = produce(personList, draft => {  // immer 활용
+      draft.push(personObj);
+    });
+    
+    setPersonList(newPersonList);
+  };
 
   return (
     <div className="container">
+      <button onClick={addPerson}>추가</button>
       <Table personList={personList} />
     </div>
   );

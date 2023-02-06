ff520e7fdf6d899a682083506ab2588821f91df0
dgk <csco0124@naver.com>
2023-02-06 23:46:06 +0900

    12

--- vue-test/1_test/vue-project/src/views/DataBinding.vue
+++ vue-test/1_test/vue-project/src/views/DataBinding.vue
@@ -1,28 +1,20 @@
 <template>
-  <div class="container" v-bind:class="[activeClass, errorClass]">Class Binding</div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      activeClass: 'active',
-      errorClass: 'text-red'
-    };
+  <div>
+   <button type="button" @click="increaseCounter">Add 1</button>
+   <p>The counter is : {{counter}} </p>
+  </div>
+  </template>
+  <script>
+  export default {
+   data() {
+     return {
+       counter: 0
+     };
+   },
+   methods: {
+     increaseCounter(){
+       this.counter = this.counter + 1;
+     }
+   }
   }
-}
-</script>
-<style scoped>
-.container {
-  width: 100%;
-  height: 200px;
-}
-
-.active {
-  background-color: yellow;
-  font-weight: bold;
-}
-
-.text-red {
-  color: red;
-}
-</style>
+  </script>

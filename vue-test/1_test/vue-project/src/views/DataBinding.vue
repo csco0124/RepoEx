db38ec229cc69d79759266a818c53a8832523278
dgk <csco0124@naver.com>
2023-02-09 01:13:24 +0900

    123

--- vue-test/1_test/vue-project/src/views/DataBinding.vue
+++ vue-test/1_test/vue-project/src/views/DataBinding.vue
@@ -1,30 +1,56 @@
 <template>
   <div>
-    <h1>Full Name : {{fullName}}</h1>
-    <button type="button" @click="changeName">변경</button>
+    <table>
+      <thead>
+        <tr>
+          <th>제품명</th>
+          <th>가격</th>
+          <th>카테고리</th>
+          <th>배송료</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="i" v-for="(product,i) in productList">
+          <td>{{ product.product_name }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.delivery_price }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  </template>
-  <script>
-  export default {
-   data() {
-     return {
-       firstName: 'Seungwon',
-       lastName: 'Go',
-       fullName: ''
-     };
-   },
-   watch: {
-     firstName() {
-       this.fullName = this.firstName + ' ' + this.lastName;
-     },
-     lastName() {
-       this.fullName = this.firstName + ' ' + this.lastName;
-     }
-   },
-   methods: {
-     changeName(){
-       this.firstName = 'Eunsol';
-     }
-   }
+</template>
+<script>
+export default {
+  data() {
+    return {
+      productList: []
+    };
+  },
+  mounted() {
+    console.log('1212');
+    this.getList();
+  },
+  methods: {
+    async getList() {
+
+      this.productList = await this.$api("https://04b4f544-7554-42de-873b-3ee82d6b5e12.mock.pstmn.io/list/", "get");
+      
+    }
   }
-  </script>
+}
+</script>
+<style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>

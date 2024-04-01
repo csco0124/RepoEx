41c9f224c4daa1e52c6ca59b452f59f3cfd743a7
dgkim <dgkim@naruint.com>
2024-04-01 14:14:47 +0900

    12

--- JWTDemo/src/main/java/com/exam/config/SecurityConfig.java
+++ JWTDemo/src/main/java/com/exam/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.exam.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +20,23 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 	private final JwtTokenProvider jwtTokenProvider;
 	
+    // 정적 자원에 대해서는 Security 설정을 적용하지 않음.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring().antMatchers("/v2/api-docs");
+            web.ignoring().antMatchers("/swagger-ui.html");
+            web.ignoring().antMatchers("/webjars/**");
+            web.ignoring().antMatchers("/swagger/**");
+            web.ignoring().antMatchers("/css/*");
+            web.ignoring().antMatchers("/js/*");
+            web.ignoring().antMatchers("/font/*");
+            web.ignoring().antMatchers("/images/*");
+            web.ignoring().antMatchers("/favicon.ico");
+            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        };
+    }
+	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -26,7 +45,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login","/test","/validateToken").permitAll()
+                .antMatchers("/","/login","/test","/validateToken").permitAll()
                 .antMatchers("/testAuth").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()

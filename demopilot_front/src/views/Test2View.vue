214700e75e848a5da8da73c936b9ccfc847127f2
dgkim <dgkim@naruint.com>
2024-04-11 16:59:14 +0900

    12

--- demopilot_front/src/views/Test2View.vue
+++ demopilot_front/src/views/Test2View.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <v-btn @click="toggleMenu">메뉴 토글 {{ menuOpen }}</v-btn>
+    <v-btn @click="toggleMenu">메뉴 토글</v-btn>
 		<v-list :opened="opened">
 			<v-list-group value="menu1">
       <template v-slot:activator="{isOpen, props}">
-				<v-list-item v-bind="props">{{ isOpen }}</v-list-item>
+				<v-list-item v-bind="props" title="menu1"></v-list-item>
       </template>
       <v-list-item>메뉴 아이템 1-1</v-list-item>
       <v-list-item>메뉴 아이템 1-2</v-list-item>
     </v-list-group>
 		<v-list-group value="menu2">
       <template v-slot:activator="{isOpen, props}">
-				<v-list-item v-bind="props">{{ isOpen }}</v-list-item>
+				<v-list-item v-bind="props" title="menu2"></v-list-item>
       </template>
       <v-list-item>메뉴 아이템 2-1</v-list-item>
       <v-list-item>메뉴 아이템 2-2</v-list-item>

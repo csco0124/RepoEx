599af6ee5b279dce52a790e7a3025e765fc979f0
dgkim <dgkim@naruint.com>
2024-04-11 16:17:25 +0900

    12

--- demopilot_front/src/views/Test2View.vue
+++ demopilot_front/src/views/Test2View.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <v-btn @click="toggleMenu">메뉴 토글 {{ menuOpen }}</v-btn>
-		<v-list>
-    <v-list-group>
+		<v-list :opened="opened">
+			<v-list-group value="menu1">
       <template v-slot:activator="{isOpen, props}">
 				<v-list-item v-bind="props">{{ isOpen }}</v-list-item>
       </template>
-      <v-list-item>메뉴 아이템 1</v-list-item>
-      <v-list-item>메뉴 아이템 2</v-list-item>
+      <v-list-item>메뉴 아이템 1-1</v-list-item>
+      <v-list-item>메뉴 아이템 1-2</v-list-item>
+    </v-list-group>
+		<v-list-group value="menu2">
+      <template v-slot:activator="{isOpen, props}">
+				<v-list-item v-bind="props">{{ isOpen }}</v-list-item>
+      </template>
+      <v-list-item>메뉴 아이템 2-1</v-list-item>
+      <v-list-item>메뉴 아이템 2-2</v-list-item>
     </v-list-group>
 	</v-list>
   </div>
@@ -17,12 +24,16 @@
 export default {
   data() {
     return {
-      menuOpen: false,
+			opened : ['menu1'],
     };
   },
   methods: {
     toggleMenu() {
-      this.menuOpen = !this.menuOpen;
+			if(this.opened[0] == 'menu1'){
+				this.opened[0] = 'menu2';
+			} else {
+				this.opened[0] = 'menu1';
+			}
     },
   }
 };

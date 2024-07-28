81c0117dcde7f578efbd263a14bea1303c132f7a
dgk <csco0124@naver.com>
2024-07-29 02:00:11 +0900

    12

--- apple-next/forum/app/page.js
+++ apple-next/forum/app/page.js
@@ -1,6 +1,11 @@
+import { connectDB } from "@/util/database";
 
-export default function Home() {
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
   return (
-    <div>ddd</div>
+    <div>{result[0].title}</div>
   );
 }

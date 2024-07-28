c6c93e4c5aedfe11e6486314108b989adde026ca
dgk <csco0124@naver.com>
2024-07-29 00:31:19 +0900

    12

--- apple-next/fresh/app/list/page.js
+++ apple-next/fresh/app/list/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
   return (
@@ -7,7 +9,7 @@ export default function List() {
         console.log(a, i);
         return (
           <div className="food" key={i}>
-            <img src="" />
+            <img src={`/food${i}.png`} className="food-img" />
             <h4>{a} $40</h4>
           </div>
         );

07c2c4e1660b863528ff5f4cd92df597429abff1
dgk <csco0124@naver.com>
2024-07-29 23:43:37 +0900

    12

---
+++ apple-next/forum/pages/api/post/new.js
@@ -0,0 +1,17 @@
+import { connectDB } from "@/util/database";
+
+export default async function handler(요청, 응답) {
+  if (요청.method == "POST") {
+    console.log("요청.body", 요청.body);
+    if (요청.body.title == "") {
+      return 응답.status(500).json("너 제목 왜 안씀");
+    }
+    try {
+      const db = (await connectDB).db("forum");
+      let result = await db.collection("post").insertOne(요청.body);
+      return 응답.status(200).redirect("/list");
+    } catch (error) { // DB저장시 에러
+
+    }
+  }
+}
--- apple-next/forum/pages/api/test.js
+++ apple-next/forum/pages/api/test.js
@@ -1,4 +1,5 @@
 export default function handler(요청, 응답) {
-  console.log(123);
-  return 응답.status(200).json("처리완료");
+  if (요청.method == "POST") {
+    return 응답.status(200).json("처리완료");
+  }
 }

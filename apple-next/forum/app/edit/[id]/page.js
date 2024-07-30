4a7a88203aee11174fc4637daf97c61aeb1f7840
dgk <csco0124@naver.com>
2024-07-30 22:01:00 +0900

    12

--- apple-next/forum/app/edit/[id]/page.js
+++ apple-next/forum/app/edit/[id]/page.js
@@ -9,10 +9,11 @@ export default async function Edit(props) {
   return (
     <div className="p-20">
       <h4>수정페이지</h4>
-      <form action="/api/post/new" method="POST">
-        <input name="title" placeholder="글제목" value={result.title} />
-        <input name="content" placeholder="글내용" value={result.content} />
-        <button type="submit">버튼</button>
+      <form action="/api/post/edit" method="POST">
+        <input name="title" placeholder="글제목" defaultValue={result.title} />
+        <input name="content" placeholder="글내용" defaultValue={result.content} />
+        <input name="_id" value={result._id} readOnly />
+        <button type="submit">전송</button>
       </form>
     </div>
   );
---
+++ apple-next/forum/pages/api/post/edit.js
@@ -0,0 +1,22 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function handler(요청, 응답) {
+  if (요청.method == "POST") {
+    console.log("요청.body", 요청.body);
+    if (요청.body.title == "") {
+      return 응답.status(500).json("너 제목 왜 안씀");
+    }
+    try {
+      let changeObj = { title: 요청.body.title, content: 요청.body.content };
+      const db = (await connectDB).db("forum");
+      let result = await db
+        .collection("post")
+        .updateOne({ _id: new ObjectId("" + 요청.body._id) }, { $set: changeObj });
+        console.log('result', result);
+      return 응답.status(200).redirect("/list");
+    } catch (error) {
+      // DB저장시 에러
+    }
+  }
+}

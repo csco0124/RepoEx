e0fd5c07bc747110af7fb46342f1c1b3c9241365
dgk <csco0124@naver.com>
2024-07-30 21:37:11 +0900

    12

--- apple-next/forum/app/edit/[id]/page.js
+++ apple-next/forum/app/edit/[id]/page.js
@@ -1,16 +1,17 @@
 import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
 
-export default async function Edit() {
-  const db = (await connectDBB).db("forum");
-  let result = await db.collection("post").findOne({ _id: new ObjectId(""+props.params.id) });
+export default async function Edit(props) {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").findOne({ _id: new ObjectId("" + props.params.id) });
   console.log(result);
 
   return (
     <div className="p-20">
       <h4>수정페이지</h4>
       <form action="/api/post/new" method="POST">
-        <input name="title" placeholder="글제목" />
-        <input name="content" placeholder="글내용" />
+        <input name="title" placeholder="글제목" value={result.title} />
+        <input name="content" placeholder="글내용" value={result.content} />
         <button type="submit">버튼</button>
       </form>
     </div>

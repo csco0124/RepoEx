a71fa557333a5363976f6cf3caae05ef267aa75c
dgk <csco0124@naver.com>
2024-07-31 20:01:38 +0900

    12

--- apple-next/forum/app/list2/page.js
+++ apple-next/forum/app/list2/page.js
@@ -1,9 +1,9 @@
 import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
-export const dynamic = "force-dynamic"; // 이 페이지를 다이나믹 렌더링으로 보여줌
+export const revalidate = 20; // 20초마다 갱신(20초 캐싱)
 
-export default async function List() {
+export default async function List2() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
   return (

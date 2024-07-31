5fa92605a779abcde5693d1fcdfb3fe2ea916a13
dgk <csco0124@naver.com>
2024-08-01 02:37:14 +0900

    12

--- apple-next/forum/app/detail/[id]/Comment.js
+++ apple-next/forum/app/detail/[id]/Comment.js
@@ -1,16 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Comment({_id}) {
+export default function Comment({ _id }) {
   let [comment, setComment] = useState("");
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/comment/list?id=" + _id).then(r => r.json())
+      .then((result) => {
+        console.log("result2", result);
+        setData(result);
+      });
+  }, []);
   return (
     <div>
       <div>댓글목록</div>
-      <input onChange={(e) => {setComment(e.target.value);}}/>
+      { 
+           data.length > 0 ?
+           data.map((a,i)=>
+             <p key={i}>{a.content}</p>
+           )
+           : '댓글없음'
+        }
+      <input
+        onChange={(e) => {
+          setComment(e.target.value);
+        }}
+      />
       <button
         onClick={() => {
-          fetch("/api/comment/new", { method: "POST", body: JSON.stringify({_id, comment}) });
+          fetch("/api/comment/new", {
+            method: "POST",
+            body: JSON.stringify({ _id, comment }),
+          });
         }}
       >
         댓글전송
--- apple-next/forum/pages/api/comment/new.js
+++ apple-next/forum/pages/api/comment/new.js
@@ -1,15 +1,19 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 export default async function handler(요청, 응답) {
-  console.log(요청.body);
-  // if (요청.method == "POST") {
-  //   let 저장할거 = {
-  //     content: 요청.body,
-  //     parent: "부모게시물_id",
-  //     author: "유저이메일",
-  //   };
-  //   let db = (await connectDB).db("forum");
-  //   let result = await db.collection("comment").insertOne(저장할거);
-  //   응답.status(200).json("저장완료");
-  // }
+  let session = await getServerSession(요청, 응답, authOptions)
+  요청.body = JSON.parse(요청.body);
+  if (요청.method == "POST") {
+    let 저장할거 = {
+      content: 요청.body.comment,
+      parent: new ObjectId(요청.body._id),
+      author: session.user.email,
+    };
+    let db = (await connectDB).db("forum");
+    let result = await db.collection("comment").insertOne(저장할거);
+    console.log('result', result);
+    응답.status(200).json("저장완료");
+  }
 }

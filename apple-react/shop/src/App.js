2f992d76a5fb84cbc8acf13230194814a333230e
dgk <csco0124@naver.com>
2024-08-04 01:28:45 +0900

    12

--- apple-react/shop/src/App.js
+++ apple-react/shop/src/App.js
@@ -1,10 +1,19 @@
-import { Button } from 'react-bootstrap';
-import './App.css';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="outline-primary">Primary</Button>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
